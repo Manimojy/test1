7428304907039640dcce0d423b50028c8cfafdc9
Manimojy <ishirraj@gmail.com>
2016-10-12 03:33:00 +0530

    add color

--- module1/js/app.js
+++ module1/js/app.js
@@ -9,7 +9,8 @@
     function LunchCheckController($scope) {
         $scope.txtEntered = "";
         $scope.msg="";
-
+        $scope.msgClass="";
+        $scope.listClass="";
         $scope.getStatus = function () {
             $scope.msg  = CalculateValueofString($scope.txtEntered);
         };
@@ -17,18 +18,22 @@
         function CalculateValueofString(str) {
             var msg="";
             if(str.length===0)
-                msg = "Please enter data first";
-            else
-            
             {
-                        var strs=str.split(',');
-                        var len = strs.length;
-                        if(len>=1 && len<=3)
-                          msg="Enjoy";
-                        else if(len>3)
-                          msg="Too much!";
+                msg = "Please enter data first";
+                $scope.msgClass="text-danger";
+                $scope.listClass="has-error";
+              }
+            else
+            {
+                var strs=str.split(',');
+                var len = strs.length;
+                if(len>=1 && len<=3)
+                    msg="Enjoy!";
+                else if(len>3)
+                    msg="Too much!";
+                $scope.msgClass="text-success";
+                $scope.listClass="has-success";
             }
-
 
             return msg;
         }
